5d70e2a100ee22831c69ed9d8c862a15cbafff01
shiponirfan <shiponirfan.dev@gmail.com>
2024-09-01 01:18:38 +0600

    Custom Responsive Builder Created

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_design_ostad/utils/app_colors.dart';
+import 'package:flutter_responsive_design_ostad/widgets/responsive_builder.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,137 +12,141 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 28),
-          child: Text(
-            'Humming\nBird .'.toUpperCase(),
-            style: const TextStyle(fontSize: 18, height: 1),
-          ),
-        )
-      ]),
-      drawer: Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              color: AppColors.primaryColor,
-              height: 200,
-              width: double.infinity,
-              child: SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Skill Up Now'.toUpperCase(),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 24,
-                      ),
-                    ),
-                    Text(
-                      'Tap Here'.toUpperCase(),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
+    return ResponsiveBuilder(
+        mobile: Scaffold(
+          appBar: AppBar(actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 28),
+              child: Text(
+                'Humming\nBird .'.toUpperCase(),
+                style: const TextStyle(fontSize: 18, height: 1),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(32),
-              child: Column(
-                children: [
-                  ListTile(
-                    title: Text(
-                      'Episodes',
-                      style: TextStyle(fontSize: 22, color: Colors.black),
+            )
+          ]),
+          drawer: Drawer(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: AppColors.primaryColor,
+                  height: 200,
+                  width: double.infinity,
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Skill Up Now'.toUpperCase(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                          ),
+                        ),
+                        Text(
+                          'Tap Here'.toUpperCase(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
                     ),
-                    leading: Icon(
-                      Icons.movie_filter_outlined,
-                      size: 28,
-                      color: Colors.black,
-                    ),
-                    minLeadingWidth: 40,
                   ),
-                  SizedBox(
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(32),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          'Episodes',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                        leading: Icon(
+                          Icons.movie_filter_outlined,
+                          size: 28,
+                          color: Colors.black,
+                        ),
+                        minLeadingWidth: 40,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ListTile(
+                        title: Text(
+                          'About',
+                          style: TextStyle(fontSize: 22, color: Colors.black),
+                        ),
+                        leading: Icon(
+                          Icons.question_answer_outlined,
+                          size: 28,
+                          color: Colors.black,
+                        ),
+                        minLeadingWidth: 40,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Flutter Web.\nThe Basics'.toUpperCase(),
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w900,
+                      height: 1,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
                     height: 20,
                   ),
-                  ListTile(
-                    title: Text(
-                      'About',
-                      style: TextStyle(fontSize: 22, color: Colors.black),
-                    ),
-                    leading: Icon(
-                      Icons.question_answer_outlined,
-                      size: 28,
+                  const Text(
+                    'In this course we will go over the basics of using Flutter Web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals and more.',
+                    style: TextStyle(
                       color: Colors.black,
                     ),
-                    minLeadingWidth: 40,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStateProperty.all(AppColors.primaryColor),
+                      shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                      padding: const WidgetStatePropertyAll(
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 10)),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Join course',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
+          ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Flutter Web.\nThe Basics'.toUpperCase(),
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 50,
-                fontWeight: FontWeight.w900,
-                height: 1,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'In this course we will go over the basics of using Flutter Web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals and more.',
-              style: TextStyle(
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    WidgetStateProperty.all(AppColors.primaryColor),
-                shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6))),
-                padding: const WidgetStatePropertyAll(
-                    EdgeInsets.symmetric(horizontal: 50, vertical: 10)),
-              ),
-              onPressed: () {},
-              child: const Text(
-                'Join course',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        desktop: Scaffold());
   }
 }
---
+++ lib/widgets/responsive_builder.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_design_ostad/utils/screen_utils.dart';
+
+class ResponsiveBuilder extends StatelessWidget {
+  const ResponsiveBuilder({
+    super.key,
+    required this.mobile,
+    required this.desktop,
+    this.tablet,
+  });
+
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final DeviceType deviceType = ScreenUtils.getDeviceType(size.width);
+
+    if (deviceType == DeviceType.mobile) {
+      return mobile;
+    } else if (deviceType == DeviceType.tablet) {
+      return tablet ?? mobile;
+    } else {
+      return desktop;
+    }
+  }
+}
