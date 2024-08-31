9e9ee0a3c1406c18d803acdea0490b766e31d497
shiponirfan <shiponirfan.dev@gmail.com>
2024-08-31 23:19:21 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/shiponirfan/flutter_responsive_design_ostad/flutter_responsive_design_ostad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shiponirfan.flutter_responsive_design_ostad.flutter_responsive_design_ostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_design_ostad/presentation/screens/home_screen.dart';
+
+void main() {
+  runApp(const ResponsiveDesignApp());
+}
+
+class ResponsiveDesignApp extends StatelessWidget {
+  const ResponsiveDesignApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
